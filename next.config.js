b3c69cd48677b098a1729578548b95096be5226c
Rasmus Hansen <rasmush92@live.com>
2023-02-19 04:41:58 +0000

    s3

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = withStoreConfig({
   images: {
     domains: [
       "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      "https://varperla-media.ams3.digitaloceanspaces.com",
+      "varperla-media.s3.amazonaws.com",
+      // "https://varperla-media.ams3.digitaloceanspaces.com",
       "localhost",
     ],
   },
