3d3825e31398ebd5f74d3e07e1ca4fd5ff187d01
Rasmus Hansen <rasmush92@live.com>
2023-02-21 00:10:00 +0000

    page under construction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,9 @@ import { CartDropdownProvider } from "@lib/context/cart-dropdown-context"
 import { MobileMenuProvider } from "@lib/context/mobile-menu-context"
 import { StoreProvider } from "@lib/context/store-context"
 import { Hydrate } from "@tanstack/react-query"
+import clsx from "clsx"
 import { CartProvider, MedusaProvider } from "medusa-react"
+import Image from "next/image"
 import "styles/globals.css"
 import { AppPropsWithLayout } from "types/global"
 
@@ -21,7 +23,33 @@ function App({
         client: queryClient,
       }}
     >
-      <div></div>
+      <div className={clsx("h-screen flex bg-[#004346]")}>
+        <div
+          className={clsx(
+            "basis-7/12 flex items-center justify-center h-screen"
+          )}
+        >
+          <div className={"flex flex-col gap-8 text-white"}>
+            <h1 className={clsx("text-7xl font-sans")}>
+              Várperla <br /> Blomuhandil
+            </h1>
+            <p
+              className={clsx("text-3xl font-sans underline decoration-white")}
+            >
+              Haldi okkum til góðar, síðan er í menning
+            </p>
+          </div>
+        </div>
+        <div className={clsx("relative flex-1 shadow")}>
+          <Image
+            src="/Flower.jpg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            className={clsx("absolute inset-0")}
+          />
+        </div>
+      </div>
     </MedusaProvider>
   )
 }
