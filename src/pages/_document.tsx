31adb42c9c4158517411a47407bba7231eac1030
Rasmus Hansen <rasmush92@live.com>
2023-04-12 23:46:39 +0100

    removed script from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,20 +23,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-        (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-            t=l.createElement(r);
-            t.async=1;
-            t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];
-            y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_KEY}");`,
-            }}
-          />
-          ;
         </body>
       </Html>
     )
