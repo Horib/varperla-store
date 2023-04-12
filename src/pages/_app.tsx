805fcd0c992fdd3b6ac0b4652e9c51b473ae0577
Rasmus Hansen <rasmush92@live.com>
2023-04-12 23:44:39 +0100

    merged develop into main, moved clairty script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,7 @@ function App({
           client: queryClient,
         }}
       >
-        <Hydrate state={pageProps.dehydratedState}>
+        {/* <Hydrate state={pageProps.dehydratedState}>
           <CartDropdownProvider>
             <MobileMenuProvider>
               <CartProvider>
@@ -54,7 +54,36 @@ function App({
               </CartProvider>
             </MobileMenuProvider>
           </CartDropdownProvider>
-        </Hydrate>
+        </Hydrate> */}
+        <div className={clsx("h-screen flex bg-[#004346]")}>
+          <div
+            className={clsx(
+              "grow xl:basis-6/12 xl:grow-0 flex items-center justify-center h-screen"
+            )}
+          >
+            <div className={"flex flex-col gap-8 text-white p-16"}>
+              <h1 className={clsx("text-7xl font-sans")}>
+                Várperla <br /> Blómuhandil
+              </h1>
+              <p
+                className={clsx(
+                  "text-3xl font-sans underline decoration-white"
+                )}
+              >
+                Haldi okkum til góðar, síðan er í gerð
+              </p>
+            </div>
+          </div>
+          <div className={clsx("relative flex-1 shadow hidden xl:block")}>
+            <Image
+              src="/Flower.jpg"
+              alt=""
+              layout="fill"
+              objectFit="cover"
+              className={clsx("absolute inset-0")}
+            />
+          </div>
+        </div>
       </MedusaProvider>
     </PlausibleProvider>
   )
