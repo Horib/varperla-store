04332f26b7a796b5513ab810cdffbe8fac57fcd5
Rasmus Hansen <rasmush92@live.com>
2023-04-13 01:11:53 +0100

    added Vercel analytics + created env toggle for what to display based on env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,15 +9,17 @@ import { CartProvider, MedusaProvider } from "medusa-react"
 import Image from "next/image"
 import "styles/globals.css"
 import { AppPropsWithLayout } from "types/global"
-import Script from "next/script"
 
 import PlausibleProvider from "next-plausible"
+import { Analytics } from "@vercel/analytics/react"
 
 function App({
   Component,
   pageProps,
 }: AppPropsWithLayout<{ dehydratedState?: unknown }>) {
   const getLayout = Component.getLayout ?? ((page) => page)
+
+  const isProduction = process.env.NODE_ENV === "production"
 
   return (
     <PlausibleProvider trackOutboundLinks={true} domain="varperla.fo">
@@ -27,48 +29,52 @@ function App({
           client: queryClient,
         }}
       >
-        {/* <Hydrate state={pageProps.dehydratedState}>
-          <CartDropdownProvider>
-            <MobileMenuProvider>
-              <CartProvider>
-                <StoreProvider>
-                  <AccountProvider>
-                    {getLayout(<Component {...pageProps} />)}
-                  </AccountProvider>
-                </StoreProvider>
-              </CartProvider>
-            </MobileMenuProvider>
-          </CartDropdownProvider>
-        </Hydrate> */}
-        <div className={clsx("h-screen flex bg-[#004346]")}>
-          <div
-            className={clsx(
-              "grow xl:basis-6/12 xl:grow-0 flex items-center justify-center h-screen"
-            )}
-          >
-            <div className={"flex flex-col gap-8 text-white p-16"}>
-              <h1 className={clsx("text-7xl font-sans")}>
-                Várperla <br /> Blómuhandil
-              </h1>
-              <p
-                className={clsx(
-                  "text-3xl font-sans underline decoration-white"
-                )}
-              >
-                Haldi okkum til góðar, síðan er í gerð
-              </p>
+        {isProduction ? (
+          <div className={clsx("h-screen flex bg-[#004346]")}>
+            <div
+              className={clsx(
+                "grow xl:basis-6/12 xl:grow-0 flex items-center justify-center h-screen"
+              )}
+            >
+              <div className={"flex flex-col gap-8 text-white p-16"}>
+                <h1 className={clsx("text-7xl font-sans")}>
+                  Várperla <br /> Blómuhandil
+                </h1>
+                <p
+                  className={clsx(
+                    "text-3xl font-sans underline decoration-white"
+                  )}
+                >
+                  Haldi okkum til góðar, síðan er í gerð
+                </p>
+              </div>
+            </div>
+            <div className={clsx("relative flex-1 shadow hidden xl:block")}>
+              <Image
+                src="/Flower.jpg"
+                alt=""
+                layout="fill"
+                objectFit="cover"
+                className={clsx("absolute inset-0")}
+              />
             </div>
           </div>
-          <div className={clsx("relative flex-1 shadow hidden xl:block")}>
-            <Image
-              src="/Flower.jpg"
-              alt=""
-              layout="fill"
-              objectFit="cover"
-              className={clsx("absolute inset-0")}
-            />
-          </div>
-        </div>
+        ) : (
+          <Hydrate state={pageProps.dehydratedState}>
+            <CartDropdownProvider>
+              <MobileMenuProvider>
+                <CartProvider>
+                  <StoreProvider>
+                    <AccountProvider>
+                      {getLayout(<Component {...pageProps} />)}
+                    </AccountProvider>
+                  </StoreProvider>
+                </CartProvider>
+              </MobileMenuProvider>
+            </CartDropdownProvider>
+          </Hydrate>
+        )}
+        <Analytics />
       </MedusaProvider>
     </PlausibleProvider>
   )
