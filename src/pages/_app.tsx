f099a72897f8d027ccc7ade1dc3dfc186fa4092e
Rasmus Hansen <rasmush92@live.com>
2023-04-13 01:00:14 +0100

    merge changes from develop and moved script back to where it should have been

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,21 +21,6 @@ function App({
 
   return (
     <PlausibleProvider trackOutboundLinks={true} domain="varperla.fo">
-      <Script
-        id="clarity-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-            t=l.createElement(r);
-            t.async=1;
-            t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];
-            y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_KEY}");`,
-        }}
-      />
       <MedusaProvider
         baseUrl={MEDUSA_BACKEND_URL}
         queryClientProviderProps={{
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { MEDUSA_BACKEND_URL } from "@lib/config"
 import Document, { Head, Html, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 class MyDocument extends Document {
   render() {
@@ -23,6 +24,21 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script
+            id="clarity-script"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);
+            t.async=1;
+            t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_KEY}");`,
+            }}
+          />
         </body>
       </Html>
     )
