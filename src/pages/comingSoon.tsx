6556fb702d25ecb49551bd4d58a7547c179a4995
Rasmus Hansen <rasmush92@live.com>
2023-04-22 19:43:06 +0100

    Code improvements and images optimization
    - Improved code quality and structure
    - Improved media queries
    - Added optimzed images for faster loading, and removed unused images
    - Added alt text to images and icons
        - Added unsplash credit to images
    - Added meta title to head
    
    Related to #13

--- src/pages/comingSoon.tsx
+++ src/pages/comingSoon.tsx
@@ -4,18 +4,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"
 import Link from "next/link"
 import SignupForm from "@modules/common/components/forms/SignupForm"
+import Head from "@modules/common/components/head"
 
 const ComingSoon = () => {
   return (
     <>
+      <Head title="Coming Soon" description="" />
       <div
         className={clsx(
-          "flex h-screen flex-row justify-center p-5 pt-10 text-white md:pt-32 lg:p-0 lg:pt-40 2xl:pt-72"
+          "flex h-screen flex-row justify-center px-5 py-8 text-white sm:pb-14 sm:pt-12 md:pt-32 lg:p-0 lg:pt-40 2xl:pt-72"
         )}
       >
         <div
           className={clsx(
-            "flex max-w-[2440px] flex-col gap-y-12 md:gap-y-20 lg:w-full lg:flex-row lg:justify-evenly"
+            "flex max-h-[500px] max-w-[2440px] flex-col justify-between  md:gap-y-20 lg:max-h-none lg:w-full lg:flex-row lg:justify-evenly"
           )}
         >
           <div
@@ -32,7 +34,7 @@ const ComingSoon = () => {
             </h1>
             <div className={clsx("relative h-24 w-24 2xl:h-48 2xl:w-48")}>
               <Image
-                src={"/Logo.png"}
+                src={"/logo_256.webp"}
                 alt=""
                 className={clsx("absolute inset-0 mx-auto")}
                 fill
@@ -45,17 +47,15 @@ const ComingSoon = () => {
           <div className={clsx("flex flex-col items-center gap-4 lg:flex-1")}>
             <h2
               className={clsx(
-                "max-w-sm text-center font-sans text-3xl font-light uppercase tracking-widest md:text-4xl  2xl:max-w-2xl 2xl:text-5xl"
+                "max-w-md text-center font-sans text-3xl font-light uppercase tracking-widest md:text-4xl  2xl:max-w-2xl 2xl:text-5xl"
               )}
             >
-              {/* Okkurt framúr er í gerð */}
-              {/* Great things are coming */}
               Gleði tykkum, her hendur okkurt
             </h2>
-            <div className={clsx("mt-3 flex flex-col gap-y-10 2xl:mt-16")}>
+            <div className={clsx("mt-3 flex flex-col gap-y-9 2xl:mt-16")}>
               <button
                 className={clsx(
-                  "rounded-md bg-white px-10 py-4 text-base text-black 2xl:px-20 2xl:py-6 2xl:text-2xl hover:bg-slate-100 active:scale-95"
+                  "rounded-md bg-white px-10 py-4 text-base text-black hover:bg-slate-100 active:scale-95 2xl:px-20 2xl:py-6 2xl:text-2xl"
                 )}
               >
                 <span
@@ -68,33 +68,37 @@ const ComingSoon = () => {
                   </span>
                 </span>
               </button>
-              <div className={clsx("flex flex-row justify-evenly text-3xl")}>
+              <div className={clsx("flex flex-row justify-evenly")}>
                 <Link
-                  className={clsx("hover:text-slate-100 hover:scale-105")}
+                  className={clsx("hover:scale-105 hover:text-slate-100")}
                   href={
                     "https://www.facebook.com/profile.php?id=100063642802861&mibextid=ZbWKwL"
                   }
+                  aria-label="Facebook link"
                 >
-                  <FontAwesomeIcon icon={faFacebook} />
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className={clsx("w-8 lg:w-10")}
+                  />
                 </Link>
                 <Link
-                  className={clsx("hover:text-slate-100 hover:scale-105")}
+                  className={clsx("hover:scale-105 hover:text-slate-100")}
                   href={
                     "https://instagram.com/varperlablomuhandil?igshid=YmMyMTA2M2Y="
                   }
+                  aria-label="Instagram link"
                 >
-                  <FontAwesomeIcon icon={faInstagram} />
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className={clsx("w-8 lg:w-10")}
+                  />
                 </Link>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <SignupForm />
-        </div>
+        {/* <div><SignupForm /></div> */}
       </div>
-      {/* background */}
-
       <div
         className={clsx(
           "fixed bottom-0 left-0 right-0 top-0 -z-10 bg-gradient-to-tl  from-[#0E1C26] via-[#2A454B] to-[#294861]"
@@ -102,8 +106,8 @@ const ComingSoon = () => {
       >
         <div className={clsx("relative ml-auto h-full w-1/2")}>
           <Image
-            src="/nahil-naseer-xljtGZ2-P3Y-unsplash-large.jpg"
-            alt=""
+            src="/coming_soon_1350_2400.webp"
+            alt={"Coming Alive - Photo by Nahil Naseer on Unsplas"}
             className={clsx("absolute inset-0")}
             fill
             style={{
