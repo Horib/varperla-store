9ae0139bf7725b378e22fbdf08741e5702b920b9
Rasmus Hansen <rasmush92@live.com>
2023-04-24 22:01:11 +0100

    Updated Text, updated form to show text that email has all ready been subscribed

--- src/lib/util/mailchimp.ts
+++ src/lib/util/mailchimp.ts
@@ -4,6 +4,19 @@ const listId = process.env.MAILCHIMP_LIST_ID ?? ''
 const dataCenter = mailchimpAPIKey.split('-')[1];
 const url = `https://${dataCenter}.api.mailchimp.com/3.0/lists/${listId}/members`;
 
+export interface MailchimpError extends Error {
+    status: number;
+    title?: string;
+    detail?: string;
+}
+
+export function createMailchimpError(status: number, title?: string, detail?: string): MailchimpError {
+    const error = new Error(title || detail) as MailchimpError;
+    error.status = status;
+    error.title = title;
+    error.detail = detail;
+    return error;
+}
 interface SubscribeResponse {
     [key: string]: any;
 }
@@ -13,18 +26,20 @@ const subscribe = async (email: string): Promise<SubscribeResponse> => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `apikey ${mailchimpAPIKey}`
+            'Authorization': `apikey ${mailchimpAPIKey}`,
         },
         body: JSON.stringify({
             email_address: email,
-            status: 'subscribed'
-        })
+            status: 'subscribed',
+        }),
     });
 
     if (response.status >= 400) {
-        throw new Error('Failed to subscribe');
+        const errorData = await response.json();
+        throw createMailchimpError(response.status, errorData.title, errorData.detail);
     }
     return response.json();
 };
 
 export default subscribe;
+
